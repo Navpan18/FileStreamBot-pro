d224cd89044d2b73f44dc6a4c6e33b76598c8a9c
Navpan18 <npanchayan.official@gmail.com>
2023-11-07 20:35:29 +0530

    main

---
+++ Adarsh/vars.py
@@ -0,0 +1,40 @@
+# (c) adarsh-goel
+import os
+from os import getenv, environ
+from dotenv import load_dotenv
+
+
+
+load_dotenv()
+
+class Var(object):
+    MULTI_CLIENT = False
+    API_ID = int('20295350')
+    API_HASH = str('805a0a86f3b382d904617d0a2fd4fb6f')
+    BOT_TOKEN = str('6397004736:AAEu1_ib8UcuFrEdOqOTf0W091bXdiIdKyA')
+    name = str(getenv('name', 'filetolinkbot'))
+    SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
+    WORKERS = int(getenv('WORKERS', '4'))
+    BIN_CHANNEL = int('-1002023739104')
+    PORT = int(getenv('PORT', 8080))
+    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
+    PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
+    OWNER_ID = set(int(x) for x in '1220356966'.split())  
+    NO_PORT = bool(getenv('NO_PORT', False))
+    APP_NAME = None
+    OWNER_USERNAME = str('navpan18')
+    if 'DYNO' in environ:
+        ON_HEROKU = True
+        APP_NAME = str('yoyo')
+    
+    else:
+        ON_HEROKU = False
+    FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
+    HAS_SSL=bool(getenv('HAS_SSL',False))
+    if HAS_SSL:
+        URL = "https://{}/".format(FQDN)
+    else:
+        URL = "http://{}/".format(FQDN)
+    DATABASE_URL = str('mongodb+srv://npanchayan:NgnVRaxNjCLVHvft@cluster0.c9bx9sh.mongodb.net/?retryWrites=true&w=majority')
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
